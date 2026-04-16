aed9b974d57e145adee11dd61133257f5cefad51
Ritav <ritav08092004@gmail.com>
2026-04-16 09:32:00 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.04.2026 16:22:37
-// Design Name: 
-// Module Name: tb_DtBCDencoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_DtBCDencoder();
 reg [9:0]a_i;
