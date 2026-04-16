8a7d41e9ca974ca9bd2494cc0adc97c1aaad51bc
Ritav <ritav08092004@gmail.com>
2026-04-16 09:31:44 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.04.2026 16:22:05
-// Design Name: 
-// Module Name: DtBCDencoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module DtBCDencoder(
     input [9:0]a_i, 
